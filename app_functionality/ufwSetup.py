f5792e92b86819b84f2dcb24b110fa6156fae19d
Markell Jefferson <kelz@Markells-MacBook-Pro.local>
2023-03-26 14:42:05 -0400

    Fixed firewall commands

--- app_functionality/ufwSetup.py
+++ app_functionality/ufwSetup.py
@@ -24,11 +24,11 @@ def firewall_setup():
         
         elif uc == 2:
             ip = ipCheck.ip_check()
-            subprocess.run([f"sudo ufw allow from {ip}"])
+            subprocess.run(["sudo", "ufw", "allow", "from", ip])
 
         elif uc == 3:
             ip = ipCheck.ip_check()
-            subprocess.run([f"sudo ufw allow from {ip} proto tcp to any port 22"])
+            subprocess.run(["sudo", "ufw", "allow", "from", ip, "proto", "tcp", "to", "any", "port", "22"])
         
         elif uc == 4:
             web_server = input("What webserver are you using? N for Nginx A for Apache: ")
@@ -36,16 +36,16 @@ def firewall_setup():
             if web_server == "N" or web_server == "n":
                 protocol = input("What protocol do you want to allow: H for HTTP S for HTTPS: ")
                 if protocol == "H" or protocol == "h":
-                    subprocess.run(["sudo ufw allow \"Nginx HTTP\""])
+                    subprocess.run(["sudo", "ufw", "allow", "\"Nginx HTTP\""])
                 elif protocol == "S" or protocol == "S":
-                    subprocess.run(["sudo ufw allow \"Nginx HTTPS\""])
+                    subprocess.run(["sudo", "ufw", "allow", "\"Nginx HTTPS\""])
 
             elif web_server == "A" or web_server == "a":
                 protocol = input("What protocol do you want to allow: H for HTTP S for HTTPS: ")
                 if protocol == "H" or protocol == "h":
-                    subprocess.run(["sudo ufw allow \"Apache HTTP\""])
+                    subprocess.run(["sudo", "ufw", "allow", "\"Apache HTTP\""])
                 elif protocol == "S" or protocol == "S":
-                    subprocess.run(["sudo ufw allow \"Apache HTTPS\""])
+                    subprocess.run(["sudo", "ufw", "allow", "\"Apache HTTPS\""])
         elif uc == 5:
             menu.show_homepage()
             menu.menu_options()
